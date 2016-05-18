164220fbdf36ec09bb773a33cb6af53a00bb7aca
Daniel Fandiño <daniel.fandino@bitsamericas.com>
2016-05-18 09:38:59 -0500

    Se añade prueba de Routing en la App

--- app/www/js/muvbe.js
+++ app/www/js/muvbe.js
@@ -1,17 +1,24 @@
-var muvbe = angular
-  .module('muvbe', [])
-  .controller("muvbeController", ['$http', muvbeController]);
+var muvbe = angular.module('muvbe', ['ngRoute']);
 
-function muvbeController($http){
+muvbe.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "muvbeController"})
+    // Pages
+    .when("/user", {templateUrl: "partials/user.html", controller: "muvbeController"})
+}]);
+
+muvbe.controller('muvbeController', function (){
   var scope = this;
 
   scope.validateLogin = function(user, password){
     if (user == 'daniel' && password == '123456'){
       scope.successLogin = true;
       scope.messageLogin = 'Gracias por Ingresar';
+      window.location = "/#/user";
     }else{
       scope.successLogin = false;
       scope.messageLogin = 'Error al ingresar, intenta con el usuario: daniel y la contraseña 123456';
     }
   }
-}
+});

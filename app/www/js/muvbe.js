459d9b61a1dda5da2617dff6ebac4eb9ff7e7e30
Daniel Fandiño <daniel.fandino@bitsamericas.com>
2016-05-18 11:37:36 -0500

    Se añade validacion de pagina user y se añade factoria de usuario

--- app/www/js/muvbe.js
+++ app/www/js/muvbe.js
@@ -3,22 +3,34 @@ var muvbe = angular.module('muvbe', ['ngRoute']);
 muvbe.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
-    .when("/", {templateUrl: "partials/home.html", controller: "muvbeController"})
+    .when("/", {templateUrl: "partials/home.html"})
     // Pages
     .when("/user", {templateUrl: "partials/user.html", controller: "muvbeController"})
 }]);
 
-muvbe.controller('muvbeController', function (){
-  var scope = this;
+muvbe.factory("user",function(){
+  return {};
+});
 
-  scope.validateLogin = function(user, password){
-    if (user == 'daniel' && password == '123456'){
-      scope.successLogin = true;
+muvbe.controller('muvbeController', function ($scope, user){
+  var scope = this;
+  scope.user = user;
+  (function validateSession(){
+    if (!scope.user.successLogin){
+      window.location = "#/";
+    }
+  })();
+
+  scope.validateLogin = function(userName, userPassword){
+    if (userName == 'daniel' && userPassword == '123456'){
+      scope.user.successLogin = true;
+      scope.user.userName = userName;
+      scope.user.userPassword = userPassword;
       scope.messageLogin = 'Gracias por Ingresar';
-      window.location = "/#/user";
+      window.location = "#/user";
     }else{
       scope.successLogin = false;
-      scope.messageLogin = 'Error al ingresar, intenta con el usuario: daniel y la contraseña 123456';
+      scope.messageLogin = 'Error al ingresar, intenta con el usuario: daniel y la contraseña: 123456';
     }
   }
 });

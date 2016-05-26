7a863858983dcfe2844890f4f14d0427afc25ae9
edesweb <edwin.mesa@bitsamericas.com>
2016-05-26 12:43:05 -0500

    plugin authi

--- app/www/script/controller/post.js
+++ app/www/script/controller/post.js
@@ -2,14 +2,14 @@ var  muvbe = angular.module('posts', []);
 
 muvbe.controller('posts', function ($scope, $http, user ){
   console.log('posts');
-  $http.get("http://muvbe.com/?rest_route=/wp/v2/posts/1").success(function(data){
+  $http.get("http://muvbe.com/?rest_route=/wp/v2/posts").success(function(data){
     //scope.listaMisComidas = respuesta.listaComidas;
     console.log(data);
-    $scope.people = data;
+    $scope.posts = data;
 
-    scope.title = data.title.rendered;
-    // scope.content = data.content.rendered;
-    scope.date = data.date;
+    // scope.title = data.title.rendered;
+    scope.content = data.content.rendered;
+    // scope.date = data.date;
 
   });
   var scope = this;

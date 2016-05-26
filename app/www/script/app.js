31cd50ac3e6a94480dc67e52c825071ad55e9d0f
edesweb <edwin.mesa@bitsamericas.com>
2016-05-26 10:32:50 -0500

    esctrutura

---
+++ app/www/script/app.js
@@ -0,0 +1,37 @@
+var muvbe = angular.module('muvbe', [
+  'ngRoute',
+  'peopleController',
+  'posts'
+]);
+
+//ROUTING
+muvbe.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {
+      templateUrl: "partials/login/home.html",
+      controller: "muvbeHomeController as mhc"
+    })
+    // SignUp
+    .when("/signup", {
+      templateUrl: "partials/login/signup.html",
+      controller: "muvbeSignUpController as msuc"
+    })
+    // User
+    .when("/user", {
+      templateUrl: "partials/login/user.html",
+      controller: "muvbeUserController as muc"
+    })
+    // Exit
+    .when("/exit", {
+      templateUrl: "partials/login/exit.html",
+      controller: "muvbeExitController as mec"
+    })
+    .when("/post", {
+      templateUrl: "partials/post/init.html",
+      controller: "posts as muc"
+    })
+    //default url
+    .otherwise({redirectTo: "/" });
+}]);
+

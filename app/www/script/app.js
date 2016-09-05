863fe5f1cd5c37d6e5e3a327a47642a11281ad69
edesweb <programarf@outlook.com>
2016-09-05 15:05:15 -0500

    Diseños  8, 9

--- app/www/script/app.js
+++ app/www/script/app.js
@@ -9,6 +9,7 @@ function onDR()
 
 var muvbe = angular.module('muvbe', [
   'ngRoute',
+  'ngAnimate',
 ]);
 
 //Variables
@@ -403,5 +404,59 @@ muvbe.filter('unique', function() {
 
       return output;
    };
+});
+
+muvbe.animation('.repeated-item', function() {
+  return {
+    enter : function(element, done) {
+      element.css('opacity',0);
+      jQuery(element).animate({
+        opacity: 1
+      }, done);
+
+      // optional onDone or onCancel callback
+      // function to handle any post-animation
+      // cleanup operations
+      return function(isCancelled) {
+        if(isCancelled) {
+          jQuery(element).stop();
+        }
+      }
+    },
+    leave : function(element, done) {
+      element.css('opacity', 1);
+      jQuery(element).animate({
+        opacity: 0
+      }, done);
+
+      // optional onDone or onCancel callback
+      // function to handle any post-animation
+      // cleanup operations
+      return function(isCancelled) {
+        if(isCancelled) {
+          jQuery(element).stop();
+        }
+      }
+    },
+    move : function(element, done) {
+      element.css('opacity', 0);
+      jQuery(element).animate({
+        opacity: 1
+      }, done);
+
+      // optional onDone or onCancel callback
+      // function to handle any post-animation
+      // cleanup operations
+      return function(isCancelled) {
+        if(isCancelled) {
+          jQuery(element).stop();
+        }
+      }
+    },
+
+    // you can also capture these animation events
+    addClass : function(element, className, done) {},
+    removeClass : function(element, className, done) {}
+  }
 });
 

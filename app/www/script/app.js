3039dbd34f71a573a9e51a9ed03e06a8ed9782b6
Daniel Fandiño <daniel.fandino@bitsamericas.com>
2016-09-09 09:49:51 -0500

    Se añade imagenes de avatar

--- app/www/script/app.js
+++ app/www/script/app.js
@@ -104,6 +104,7 @@ muvbe.controller('muvbeController', function ($scope, $http){
           post.ubicationName = "Sin Ubicacion";
         }
         post.mediaId = data[post_data].featured_media;
+        scope.getAuthorAvatarByPost(post);
         scope.getImageUrlByPost(post, data[post_data].featured_media);
         scope.getCommentsByPost(post);
         scope.getCountFavoritesByPost(post);
@@ -185,6 +186,7 @@ muvbe.controller('muvbeController', function ($scope, $http){
   }
 
   scope.getIsFavorite = function(post){
+    scope.user = JSON.parse(localStorage.getItem("userSession"));
     var userFavorites = scope.user.favorites;
     if (userFavorites.indexOf(post.id.toString()) != -1){
       post.isFavorite = true;
@@ -208,7 +210,7 @@ muvbe.controller('muvbeController', function ($scope, $http){
           var dateComment = new Date(comments[comments_data].date);
           commentInfo.date = dateComment.getDate() + " de " + monthNames[dateComment.getMonth()] + " del " + dateComment.getFullYear();
           commentInfo.content = comments[comments_data].content.rendered;
-          post.comments.push(commentInfo);
+          scope.getAuthorAvatarByComment(post, commentInfo, comments[comments_data].author);;
         }
       }
     }
@@ -251,6 +253,24 @@ muvbe.controller('muvbeController', function ($scope, $http){
         }
       }
     }
+  }
+
+  scope.getAuthorAvatarByPost = function(post){
+    $http.get(urlAppServer2 + '/user/get_userinfo/?user_id=' + post.author + '&insecure=cool').success(function(dataAvatar){
+      post.authorAvatar = dataAvatar.avatar;
+      localStorage.setItem("posts", JSON.stringify(scope.posts));
+    });
+  }
+
+  scope.getAuthorAvatarByComment = function(post, comment, author){
+    $http.get(urlAppServer2 + '/user/get_userinfo/?user_id=' + author + '&insecure=cool').success(function(dataAvatar){
+      console.log(author);
+      comment.authorAvatar = dataAvatar.avatar;
+      console.log(comment.authorAvatar);
+      post.comments.push(comment);
+      localStorage.setItem("comments", JSON.stringify(scope.comments));
+      localStorage.setItem("posts", JSON.stringify(scope.posts));
+    });
   }
 
   scope.addFavorite = function(postId){

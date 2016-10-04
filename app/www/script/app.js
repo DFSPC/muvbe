4a7a736bace3346c5befc0c9beaf1881d64057af
edesweb <programarf@outlook.com>
2016-10-04 08:41:24 -0500

    cambio del texto cargado

--- app/www/script/app.js
+++ app/www/script/app.js
@@ -26,7 +26,7 @@ var $body = angular.element('body');
 
 function load() {
   $body.addClass('overflow')
-  $body.append('<div class="load"><div class="wraper"></div><div/>');
+  $body.append('<div class="load"><div class="wraper"><p>Cargando...</p></div><div/>');
 
 }
 function finishedLoad(){
@@ -146,7 +146,7 @@ muvbe.controller('muvbeController', function ($scope, $http){
   }
 
   scope.getAllData = function(){
-    scope.messageData = "¡ CARGANDO !";
+    // scope.messageData = "Cargando...";
     load();
     scope.posts = new Array();
     $http.get(urlAppServer + "/categories?per_page=100").success(function(data){
